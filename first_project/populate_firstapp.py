9599a418704c622dfea3dd8413a004bebb2fd6e4
Shubham Singhal <shubham.011190@gmail.com>
2019-02-20 10:51:28 +0530

    updated

--- first_project/populate_firstapp.py
+++ first_project/populate_firstapp.py
@@ -25,9 +25,7 @@ def populate(N=5):
         fakeDate=fakegen.date()
 
         webpg=WebPage.objects.get_or_create(topic=top, name=fakeName,url=fakeUrl)
-        webpg.save()
         accessRec=AccessRecord.objects.get_or_create(name=fakeName,date=fakeDate)
-        accessRec.save()
 
 if __name__=='__main__':
     print('Populating !')
