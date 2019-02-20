4bb83eb2b7171aeeef422442acabb8afb5ce7562
Shubham Singhal <shubham.011190@gmail.com>
2019-02-20 16:57:27 +0530

    MTV Paradigm

--- Project1/first_project/urls.py
+++ Project1/first_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from firstapp import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
+    path('firstapp/',include('firstapp.urls'))
 ]
--- Project1/firstapp/views.py
+++ Project1/firstapp/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from firstapp.models import Name,Data
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    urlList=Data.objects.order_by('date')
+    date_dict={'dates':'urlList'}
+    return render(request,'firstapp/index.html',context=date_dict)
